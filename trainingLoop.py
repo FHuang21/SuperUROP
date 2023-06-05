1475b2bec3a286feef44bb1921a3eadb677a4a66
Simon Cadavid <scadavid@netmitgpu03.csail.mit.edu>
2023-06-05 18:35:55 -0400

    found patient changes

--- trainingLoop.py
+++ trainingLoop.py
@@ -8,6 +8,26 @@ import sklearn.metrics
 from dataLoader2 import MultiClassDataset
 from tqdm import tqdm
 from ipdb import set_trace as bp
+# to remove:
+import numpy as np
+
+def perf_measure(y_actual, y_hat):
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
+
+    for i in range(len(y_hat)): 
+        if y_actual[i]==y_hat[i]==1:
+           TP += 1
+        if y_hat[i]==1 and y_actual[i]!=y_hat[i]:
+           FP += 1
+        if y_actual[i]==y_hat[i]==0:
+           TN += 1
+        if y_hat[i]==0 and y_actual[i]!=y_hat[i]:
+           FN += 1
+
+    return(TP, FP, TN, FN)
 
 torch.cuda.empty_cache() # since i usually stop the program in the middle of training before stuff's been cleared
 
@@ -21,10 +41,10 @@ train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False) # don't shuffle cause not training on the test set
 
 X_test, y_test = default_collate(testset)
-X_test = X_test.cuda()
+#X_test = X_test.cuda()
 
 # initialize model that is modification of resnet50 architecture
-resnet_layer = resnet50(weights=ResNet50_Weights.DEFAULT)#.cuda()
+resnet_layer = resnet50(weights=ResNet50_Weights.DEFAULT)
 num_features = resnet_layer.fc.in_features  # Get the number of input features for the last layer
 #num_classes = 3
 num_classes = 2
@@ -33,16 +53,51 @@ conv_layer = nn.Conv2d(3, 3, kernel_size=(1,7), stride=(1,3), padding=(0,3))
 pool_layer = nn.MaxPool2d((1,2))
 model = nn.Sequential(conv_layer, pool_layer, resnet_layer).cuda()
 
+#######
+
+y_preds = []
+y_truths = []
+for X_batch, y_batch in tqdm(train_loader):
+
+    X_batch = X_batch.cuda()
+    y_batch = y_batch.cuda()
+    y_pred = model(X_batch)
+
+    y_pred = y_pred.detach().cpu()
+    y_batch = y_batch.cpu()
+
+    y_preds.append(y_pred.argmax(dim=1))
+    y_truths.append(y_batch)
+
+y_preds = torch.cat(y_preds)
+y_truths = torch.cat(y_truths)
+
+y_preds = y_preds.detach().cpu().numpy()
+y_truths = y_truths.detach().cpu().numpy()
+
+#model.cpu()
+#model.eval()
+
+
+# X_train, y_train = default_collate(trainset)
+# y_train = y_train.detach().cpu().numpy()
+print("f1 score: ", sklearn.metrics.f1_score(y_truths, y_preds, average='macro'))
+
+
+#######
+
+bp()
+
 # 1.0, 3.0, 3.0
 # 1.12, 34.42, 11.14
 # 1.0583005244, 5.8668560575, 3.3376638537
 #class_weights = torch.tensor([1.058, 5.87, 3.34]).cuda()
-binary_class_weights = torch.tensor([1.0, 3.0]).cuda()
+binary_class_weights = torch.tensor([1.0, 9.0]).cuda()
 loss_fn = nn.CrossEntropyLoss(weight=binary_class_weights)
 optimizer = optim.Adam(model.parameters(), lr=1e-6)
 
 running_loss = 0.0
-num_epochs = 50
+num_epochs = 7
 for epoch in tqdm(range(num_epochs)):
 
     model.train()
@@ -77,7 +132,7 @@ for epoch in tqdm(range(num_epochs)):
     print("num y_truths: ", y_truths.sum())
     
     #y_preds = torch.argmax(y_preds, dim=1)
-    #X_train, y_train = default_collate(trainset)
+    
     y_preds = y_preds.detach().cpu().numpy()
     y_truths = y_truths.detach().cpu().numpy()
     accuracy = sklearn.metrics.accuracy_score(y_truths, y_preds)
@@ -90,12 +145,18 @@ for epoch in tqdm(range(num_epochs)):
     average_loss = running_loss / len(train_loader)
     running_loss = 0.0
 
+    # print("y_truths:")
+    # print(y_truths)
+    # print("y_preds")
+    # print(y_preds)
+
     print("TRAINING METRICS:")
     print("Accuracy: ", accuracy)
     print("Precision: ", precision)
     print("Recall: ", recall)
     print("Average Loss: ", average_loss)
     print("F1: ", f1)
+    print("TP/TN/FP/FN:", perf_measure(y_truths, y_preds))
 
     model.eval()
     with torch.no_grad():
